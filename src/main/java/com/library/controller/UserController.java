581c79002f110aaf4fab3250a153001cfa1d6c27
Sushil845 <sushil.0202020@gmail.com>
2026-03-20 02:34:03 +0530

    tes60

--- src/main/java/com/library/controller/UserController.java
+++ src/main/java/com/library/controller/UserController.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins ="*")
 public class UserController {
 
     @Autowired
